86b46c81c2920923a7777a1184b14943d91844c3
Atis (Laptop) <mathismewes@gmx.de>
2024-07-12 21:48:35 +0200

    More Player functionality

--- Assets/Code/Player/Player.cs
+++ Assets/Code/Player/Player.cs
@@ -3,20 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
+    public enum PlayerState {
+        walking,
+        rudder,
+        sail
+    }
+    
+    
     [Header("Player Stuff")]
     [SerializeField] private float playerSpeed = 1;
 
     [SerializeField] private Rigidbody2D myRb;
     [SerializeField] private char playerType = 'A';
-    
+    public PlayerState playerState;
+    public bool onStation = false;
     
     // Update is called once per frame
     void Update()
     {
         Vector2 inputVec = GetInput();
-        MovePlayer(inputVec);
+        if (playerState == PlayerState.walking)
+        {
+            MovePlayer(inputVec);
+        }
         // AnimatePlayer(inputVec);
     }
 
@@ -33,5 +45,11 @@ public class Player : MonoBehaviour
     private void AnimatePlayer(Vector2 direction)
     {
         throw new NotImplementedException();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!other.CompareTag("Station")) return;
+        Debug.Log(other.gameObject.name);
     }
 }

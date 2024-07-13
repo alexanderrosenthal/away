d2cbac5152e8fadd5fe66f2cc93bcecaf4b03fcb
LeanderMecklenburg <leander.mecklenburg@googlemail.com>
2024-07-13 14:01:40 +0200

    Adding Start UI

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool isGamePaused = true;
+
+    // Start the game
+    public void StartGame()
     {
-        
+        Time.timeScale = 1; // Resume the game
+        isGamePaused = false;
+        Debug.Log("Game Started");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Stop the game
+    public void StopGame()
     {
-        
+        Time.timeScale = 0; // Pause the game
+        isGamePaused = true;
+        Debug.Log("Game Stopped");
+    }
+
+    // Toggle game state
+    public void ToggleGame()
+    {
+        if (isGamePaused)
+        {
+            StartGame();
+        }
+        else
+        {
+            StopGame();
+        }
     }
 }

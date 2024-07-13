569d097cbc8d8626ec5122e5265168e07ae1c5f8
Atis (Laptop) <mathismewes@gmx.de>
2024-07-13 14:58:00 +0200

    refactoring player movement

--- Assets/Code/Player/PlayerControllerOld.cs
+++ Assets/Code/Player/PlayerControllerOld.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerOld : MonoBehaviour
 {
     [Header("Player Stuff")]
     [SerializeField] private float playerSpeed = 1;
+
+    [SerializeField] private Rigidbody2D myRb; // TODO settle for RB or no RB solution
+    [SerializeField] private Rigidbody2D boatRb;
     public char playerType = 'A';
     public bool onStation = false;
     public GameObject currentStation;
     [SerializeField] private Vector2 inputVec;
-    [SerializeField] private GameObject playerSprite;
 
     private float lookingAngle;
     // Update is called once per frame
@@ -23,7 +25,7 @@ public class PlayerController : MonoBehaviour
         {
             inputVec.x = 0; // TODO ANIMATION
             inputVec.y = 0;
-            // transform.position = currentStation.transform.position; // TODO quick workaround for the rrigidbody problem
+            transform.position = currentStation.transform.position; // TODO quick workaround for the rrigidbody problem
         }
         else
         {
@@ -50,13 +52,13 @@ public class PlayerController : MonoBehaviour
         }
   
         // Set the rotation of the object
-        playerSprite.transform.rotation = Quaternion.Euler(0, 0, lookingAngle * Mathf.Rad2Deg);
+        transform.rotation = Quaternion.Euler(0, 0, lookingAngle * Mathf.Rad2Deg);
         // transform.rotation = Quaternion.LookRotation(inputVec, Vector3.back);
     }
     private void MovePlayer()
     {
-        // myRb.velocity = boatRb.velocity + inputVec.normalized * (100f * playerSpeed * Time.deltaTime);
-        transform.Translate(inputVec.normalized * (playerSpeed * Time.deltaTime));
+        myRb.velocity = boatRb.velocity + inputVec.normalized * (100f * playerSpeed * Time.deltaTime);
+        // transform.Translate(inputVec.normalized * (playerSpeed * Time.deltaTime));
     }
     private void AnimatePlayer(Vector2 direction)
     {

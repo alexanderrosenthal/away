c428c7dccad774a76b12dc1825ec5d024b5bc6e1
Atis (Laptop) <mathismewes@gmx.de>
2024-07-13 18:07:01 +0200

    changed wind verhalten

--- Assets/Code/Stations/WindManager.cs
+++ Assets/Code/Stations/WindManager.cs
@@ -9,45 +9,48 @@ public class WindManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI windText;
     [SerializeField] private Image windRose;
     public float windDirection;
-    private float interval = 0f;
+    private float interval = 0.05f;
     public float minWindDirection = -50f;
     public float maxWindDirection = 50f;
-    [SerializeField] private float minTimeInterval = 1f;
-    [SerializeField] private float maxTimeInterval = 5f;
+    // [SerializeField] private float minTimeInterval = 1f;
+    // [SerializeField] private float maxTimeInterval = 5f;
+    [SerializeField] private float minChangeSpeed = -4f;
+    [SerializeField] private float maxChangeSpeed = 4f;
     void Start()
     {
         // Start the coroutine to update wind direction every 'interval' seconds
         StartCoroutine(UpdateWindDirection());
     }
-
+    
     IEnumerator UpdateWindDirection()
     {
         while (true)
         {
             CalculateWindDirection();
-            UpdateUIText(windDirection);
-            UpdateWindRose(windDirection);
+            UpdateUIText();
+            UpdateWindRose();
             yield return new WaitForSeconds(interval);
         }
     }
     
-    float CalculateWindDirection()
+    private void CalculateWindDirection()
     {
         // Calculate wind direction
-        windDirection = Random.Range(minWindDirection, maxWindDirection);
-        interval = Random.Range(minTimeInterval, maxTimeInterval);
-        return windDirection;
+        windDirection += Random.Range(minChangeSpeed, maxChangeSpeed);
+        windDirection = Mathf.Clamp(windDirection, minWindDirection, maxWindDirection);
+        // interval = Random.Range(minTimeInterval, maxTimeInterval);
+        // return windDirection;
     }
 
-    private void UpdateUIText(float windDirection)
+    private void UpdateUIText()
     {
         string text = "Wind Direction: " + windDirection;
         windText.text = text;
     }
     
-    private void UpdateWindRose(float windDirection)
+    private void UpdateWindRose()
     {
         // Rotate the wind rose to show the wind direction
-        windRose.transform.rotation = Quaternion.AngleAxis((windDirection), Vector3.back);
+        windRose.transform.rotation = Quaternion.AngleAxis(windDirection, Vector3.back);
     }
 }

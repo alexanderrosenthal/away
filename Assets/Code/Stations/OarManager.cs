8dfbabc2dd758b2e1e778e7679134063083aeb0d
Atis (Laptop) <mathismewes@gmx.de>
2024-07-13 18:56:33 +0200

    Added rudder

---
+++ Assets/Code/Stations/OarManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OarManager : StationManager
+{
+    [SerializeField] private float strength = 1f;
+    [SerializeField] private float strokeSeconds = 2f;
+    [SerializeField] private bool usingOar = false;
+    [SerializeField] private Rigidbody2D boatRb;
+    [SerializeField] private GameObject forcePoint;
+    
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+        if (input.y == 0) return;
+        if (usingOar) return;
+        usingOar = true;
+        if (input.y > 0)
+        {
+            StartCoroutine(ForwardStroke());
+        }
+        else
+        {
+            
+            StartCoroutine(BackwardStroke());
+        }
+
+    }
+
+    IEnumerator ForwardStroke()
+    {
+        Debug.Log("Adding rudder force");
+        boatRb.AddForceAtPosition(Vector2.up * strength, forcePoint.transform.position);
+        yield return new WaitForSeconds(strokeSeconds);
+        usingOar = false;
+    }
+
+    IEnumerator BackwardStroke()
+    {
+        boatRb.AddForceAtPosition(Vector2.down * strength, forcePoint.transform.position);
+        yield return new WaitForSeconds(strokeSeconds);
+        usingOar = false;
+    }
+}

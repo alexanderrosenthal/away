c0f14b3504e35f98ca8a82afa176354e67db6c6b
Atis (Laptop) <mathismewes@gmx.de>
2024-07-14 14:41:36 +0200

    CrowsNest starts CameraZoom()... also added Gull to MainCamera prefab

--- Assets/Code/Stations/CrowsNest.cs
+++ Assets/Code/Stations/CrowsNest.cs
@@ -1,13 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CrowsNest : StationManager
 {
-    
+    private bool lastUsed;
+    [SerializeField] private CameraZoom camZoom;
+
+    private void Start()
+    {
+        lastUsed = stationUsed;
+    }
+
     public override void Update()
     {
         base.Update();
-        // TODO animateCamera ToggleZoom(){};
+        if (lastUsed != stationUsed)
+        {
+            Debug.Log("Attempt toggle");
+            camZoom.ToggleZoom();
+            lastUsed = stationUsed;
+        }
     }
 }

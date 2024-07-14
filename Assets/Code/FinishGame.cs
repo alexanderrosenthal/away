4462c79591b959f53138a9f73b8feb361020145e
LeanderMecklenburg <leander.mecklenburg@googlemail.com>
2024-07-14 13:45:22 +0200

    Adding finished game

--- Assets/Code/FinishGame.cs
+++ Assets/Code/FinishGame.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class FinishGame : MonoBehaviour
 {
+    [SerializeField] private GameObject finishedFirework;
+    [SerializeField] private GameObject finishedUI;
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Triggered by " + other.gameObject.name);
         if (other.CompareTag("Player")) ;
         {
             Debug.Log("Player entered the trigger!");
+            finishedFirework.SetActive(true);
         }
     }
 }

926b0f95a88a77e95e4f7a727ce0fa564f4da95f
Atis (Laptop) <mathismewes@gmx.de>
2024-07-14 17:25:56 +0200

    last changes

--- Assets/Code/Player/PlayerCollision.cs
+++ Assets/Code/Player/PlayerCollision.cs
@@ -18,6 +18,7 @@ public class PlayerCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("playerCollision");
         connectionLine = playerA.transform.position - playerB.transform.position;
         distance = connectionLine.magnitude;
         if (distance < collisionDistance)

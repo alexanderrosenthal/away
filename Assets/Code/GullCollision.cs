222b262654bc186a4463b6ba30afcc8f739b7649
Atis (Laptop) <mathismewes@gmx.de>
2024-07-14 12:49:58 +0200

    Worked on gull collision

--- Assets/Code/GullCollision.cs
+++ Assets/Code/GullCollision.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GullCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool hasKicked;
+    [SerializeField] private float speed;
+    [SerializeField] private float effectSeconds;
+    private GameObject hitObj;
+    private Vector3 direction;
+    private GullMovement gullMovement;
+    private void Start()
     {
-        
+        gullMovement = GetComponent<GullMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (hasKicked) KickObject();
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered by " + other.gameObject.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            hitObj = other.gameObject;
+            hasKicked = true;
+            direction = hitObj.transform.position - transform.position;
+            direction.Normalize();
+            StartCoroutine(KickPlayerTimer());
+        }
+    }
+
+    void KickObject()
+    {
+        hitObj.transform.Translate(direction * (speed * Time.deltaTime));
+    }
+
+    IEnumerator KickPlayerTimer()
+    {
+        yield return new WaitForSeconds(effectSeconds);
+        gullMovement.killGull();
         
     }
+    
 }

47bea07c8d304dd522ce7dce42583ff3f6c4e9bd
Atis (Laptop) <mathismewes@gmx.de>
2024-07-14 14:25:12 +0200

    ..

--- Assets/Code/Stations/CrowsNest.cs
+++ Assets/Code/Stations/CrowsNest.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrowsNest : MonoBehaviour
+public class CrowsNest : StationManager
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public override void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Update();
+        // TODO animateCamera
     }
 }

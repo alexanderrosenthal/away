533267ddd058809a4bd9627817aeb50e08eb18c3
S4NGISKISTE\S4ngi <alexander_rosenthal@web.de>
2024-12-31 15:05:54 +0100

    Fix fürs Crownest - Spritelevel jetzt dynamisch

--- Assets/Code/Boat/Stations/CrowsNest.cs
+++ Assets/Code/Boat/Stations/CrowsNest.cs
@@ -8,28 +8,36 @@ public class CrowsNest : StationManager
     [Header("CrowsNest:")]
     private bool lastUsed;
     [SerializeField] private CameraZoom camZoom;
+    SpriteRenderer spriteRenderer;
+    private int playerSortingOrder;
 
     public override void Start()
     {
         base.Start();
         lastUsed = onStation;
+        spriteRenderer = GameObject.Find("PlayerA").transform.GetChild(0).GetComponent<SpriteRenderer>();
+        playerSortingOrder = spriteRenderer.sortingOrder;
     }
     public override void Update()
     {
         base.Update();
+
         if (lastUsed != onStation)
         {
             Debug.Log("Attempt toggle");
             camZoom.ToggleZoom();
 
-            //Anpassen des Sprte Render "Order in Layer"
-            playerThatEntered.transform.GetChild(0).GetComponent<SpriteRenderer>().sortingOrder = 4;
+            //Anpassen des Sprite Render "Order in Layer";
+            if (playerSortingOrder == spriteRenderer.sortingOrder)
+            {
+                spriteRenderer.sortingOrder -= 3;
+            }
 
             lastUsed = onStation;
         }
         else if (lastUsed = onStation)
         {
-            playerThatEntered.transform.GetChild(0).GetComponent<SpriteRenderer>().sortingOrder = 7;
+            spriteRenderer.sortingOrder += 3;
         }
     }
 }
